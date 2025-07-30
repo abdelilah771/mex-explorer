4bf8abf3a67d6fed9e81c1cae50c136bbf9451a7
abdelilah777 <abdelilahdahou777@gmail.com>
2025-07-30 17:48:20 +0100

    fix Dynamic APIs are Asynchronous

--- app/api/friends/accept/[requestId]/route.ts
+++ app/api/friends/accept/[requestId]/route.ts
@@ -5,7 +5,8 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function POST(request: Request, { params }: { params: { requestId: string } }) {
+export async function POST(request: Request, props: { params: Promise<{ requestId: string }> }) {
+  const params = await props.params;
   const session = await getServerSession(authOptions);
   const currentUserId = session?.user?.id;
 
